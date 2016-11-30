d2aae3f0f825f63b3e06cb2c4b5baf653ab7617b
ms@schaemicon.de <ms@schaemicon.de>
2016-11-30 20:38:41 +0100

    Projektdateien hinzufügen.

---
+++ WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        Me.OpenFileDialog1.Filter = """CSV-Dateien|*.csv|Alle Dateien|*.*"""
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(13, 13)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Select File"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(94, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(547, 18)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Label2"
+        '
+        'ListView1
+        '
+        Me.ListView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ListView1.Location = New System.Drawing.Point(13, 43)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(573, 341)
+        Me.ListView1.TabIndex = 4
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ProgressBar1.Location = New System.Drawing.Point(13, 396)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(573, 23)
+        Me.ProgressBar1.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(598, 431)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents ProgressBar1 As ProgressBar
+End Class
---
+++ WindowsApplication2/Form1.vb
@@ -0,0 +1,47 @@
+﻿Imports System.IO
+Imports System.Text
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim dateiname As String = String.Empty
+        Dim enc As Encoding = Encoding.Default
+        Dim csv As List(Of String) = New List(Of String)
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            dateiname = OpenFileDialog1.FileName
+            Label1.Text = dateiname
+        End If
+
+        Dim query = From Line In File.ReadAllLines(dateiname, enc)
+                    Let fields = Line.Split(";")
+                    Select fields
+
+        Dim customer = query.ToList()
+
+        Label2.Text = customer.Count.ToString
+        ProgressBar1.Maximum = customer.Count
+
+        Dim ColumnCount = customer(0).Count
+        For i As Integer = 0 To ColumnCount - 1
+            Debug.Print(customer(0).ElementAt(i).ToString)
+            Dim ch As New ColumnHeader
+            ch.Name = customer(0).ElementAt(i)
+            ch.Text = customer(0).ElementAt(i)
+            ListView1.Columns.Add(ch)
+        Next
+
+        For j As Integer = 1 To customer.Count - 1
+            Dim listItem As New ListViewItem(customer(j).ElementAt(0))
+            For k As Integer = 1 To ColumnCount - 1
+                listItem.SubItems.Add(customer(j).ElementAt(k))
+            Next
+            ListView1.Items.Add(listItem)
+            ProgressBar1.Value += 1
+        Next
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListView1.View = View.Details
+        ListView1.AllowColumnReorder = True
+        ListView1.GridLines = True
+    End Sub
+End Class
